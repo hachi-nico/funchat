6631648021817af74f1f30927f5a5f052241c7e6
hachi-nico <nicokun48@gmail.com>
2021-09-19 20:56:34 +0700

    add login pages

---
+++ src/color.js
@@ -0,0 +1,5 @@
+const color = {
+  smoothBlack: '#424242',
+};
+
+export default color;
---
+++ src/pages/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import color from '../color';
+
+const Login = () => {
+  return (
+    <div id="login-page">
+      <div id="login-card">
+        <h2 style={{ color: color.smoothBlack }}>Welcome to funchat</h2>
+        <button className="login-button google">
+          <GoogleOutlined /> Sign In with Google
+        </button>
+        <br />
+        <br />
+        <button className="login-button facebook">
+          <FacebookOutlined /> Sign In with Facebook
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
